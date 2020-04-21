eaf254284d8b0c7b002907b3c4d157935cb67bd9
lenovo <1156652810@qq.com>
2020-04-21 20:10:16 +0800

    郭志伟

---
+++ src/main/java/cn/jiyun/pojo/Student.java
@@ -0,0 +1,15 @@
+package cn.jiyun.pojo;
+
+public class Student {
+    private String userName;
+    private String sex;
+    //alt+fn+insert  get和set方法
+    //alt+enter  智能提示
+    public String getUserName() {
+        return userName;
+    }
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+}
