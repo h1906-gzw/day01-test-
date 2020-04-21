2f2d59b45616f3fae9e2e91d89a09859a614dd51
lenovo <1156652810@qq.com>
2020-04-21 20:19:48 +0800

    呵呵

--- src/main/java/cn/jiyun/pojo/Student.java
+++ src/main/java/cn/jiyun/pojo/Student.java
@@ -3,6 +3,8 @@ package cn.jiyun.pojo;
 public class Student {
     private String userName;
     private String sex;
+    private int age;
+    private String hobby;
     //alt+fn+insert  get和set方法
     //alt+enter  智能提示
     public String getUserName() {
